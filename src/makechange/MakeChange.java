da18733e86cbf7e0ccc031cc74aa2138d97af301
garveymt <garvey.morgant@gmail.com>
2021-01-15 18:19:51 -0700

    added guest money and less amount (still need to input the money back feature/remaining total addition)

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -8,14 +8,25 @@ public class MakeChange {
 		Scanner kb = new Scanner(System.in);
 		
 		double itemPrice = 0.0;
+		double guestMoney = 0.0;
 		
+
 		System.out.println("Welcome to cash register. Please"
-				+ " input the price of your item:");
+				+ " enter the price of your item: ");
 		itemPrice = kb.nextDouble();
+		System.out.println("Please enter the amount for your item: ");
+		guestMoney = kb.nextDouble();
 		
+		if(itemPrice == guestMoney) {
+			System.out.println("You provided the exact amount.");
+		} 
+		if (itemPrice != guestMoney) {
+			System.out.println("You entered less than the amount stated. "
+					+ "Please the remaining total of: ");
+		}
 		
-		System.out.println("Your change will be " + "Thank you for shopping"
-				+ " with us.");
+		//System.out.println("Your change will be ");
+		System.out.println("Thank you for shopping with us.");
 		
 
 	}
