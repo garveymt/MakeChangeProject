dae853644a6debc0d58a3839fb46246ad05a2426
garveymt <garvey.morgant@gmail.com>
2021-01-15 20:38:42 -0700

    created remainder system (not including change) and placeholders for money

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -9,7 +9,12 @@ public class MakeChange {
 		
 		double itemPrice = 0.0;
 		double guestMoney = 0.0;
-		
+		double remainder = 0.0;
+		double restMoney = 0.0;
+		int tenDoll; int fiveDoll; int oneDoll;
+		double quarter; double dime; double nickle; double penny;
+	
+		//double change;
 
 		System.out.println("Welcome to cash register. Please"
 				+ " enter the price of your item: ");
@@ -20,15 +25,23 @@ public class MakeChange {
 		if(itemPrice == guestMoney) {
 			System.out.println("You provided the exact amount.");
 		} 
+		if(itemPrice < guestMoney) {
+			System.out.println("Your change will be: ");
+		} 
+		
+		//limit needs to be made 20
+		
 		if (itemPrice != guestMoney) {
+			remainder = itemPrice - guestMoney;
 			System.out.println("You entered less than the amount stated. "
-					+ "Please the remaining total of: ");
+					+ "Please enter the remaining total of: " + remainder);
+			restMoney = kb.nextDouble();
+			
+			
 		}
 		
 		//System.out.println("Your change will be ");
 		System.out.println("Thank you for shopping with us.");
-		
-
 	}
 
-}
+	}
