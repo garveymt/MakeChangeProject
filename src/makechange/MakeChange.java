8e21877ab62e49052cc034285df0ec2bd6ce85c8
garveymt <garvey.morgant@gmail.com>
2021-01-17 16:56:35 -0700

    final product

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -9,106 +9,75 @@ public class MakeChange {
 		
 		double itemPrice = 0.0;
 		double guestMoney = 0.0;
-		double twentyDoll = 20, tenDoll = 10, fiveDoll = 5, oneDoll = 1;
-		double quarter = 0.25, dime = 0.10, nickel =0.05, penny = 0.01;
-		double change = 0.0, moneyBack;
+		int twentyDoll = 2000, tenDoll = 1000, fiveDoll = 500, oneDoll = 100;
+		int quarter = 25, dime = 10, nickel = 5, penny = 1;
+		double change = 0;
 		
 
 		System.out.println("Welcome to cash register. Please"
 				+ " enter the price of your item: ");
 		itemPrice = kb.nextDouble();
-		System.out.println("Please enter the amount for your item: ");
+		System.out.println("Please insert currency: ");
 		guestMoney = kb.nextDouble();
+		boolean changeGiven = false;
 		
+		while(!changeGiven) {
 		
+			if(itemPrice == guestMoney) {
+				System.out.println("You provided the exact amount.");
+			} 
 		
-		if(itemPrice == guestMoney) {
-			System.out.println("You provided the exact amount.");
-		} 
-		
-		if(itemPrice < guestMoney) {
-			change = guestMoney - itemPrice;
-			System.out.println("Your change will be: " + change);
-			
-			
-			double changeBack =(double)(Math.floor(change * 100));
-			
-			double Twenty = changeBack / twentyDoll;
-				changeBack = changeBack % twentyDoll;
+			else if(itemPrice < guestMoney) {
+				change = (guestMoney - itemPrice) * 100;
+				System.out.println("Your change will be: " + change / 100);
+				int changeBack = (int)Math.ceil(change);
+				int twenties = 0;
+				twenties = changeBack / twentyDoll;
+					changeBack = changeBack % twentyDoll;
+				System.out.println(twenties + " twenty dollar bill(s)");
 				
-			moneyBack = guestMoney - itemPrice + 0.0005;
-			System.out.println(Twenty + " twenty dollar bill(s)");
-			
-			double Ten = changeBack / tenDoll;
+				int tens = 0;
+				tens = changeBack / tenDoll;
 				changeBack = changeBack % tenDoll;
-			System.out.println(Ten + " ten dollar bill(s)");
-			
-			double Five = changeBack / fiveDoll;
+				System.out.println(tens + " ten dollar bill(s)");
+				
+				int fives = 0;
+				fives = changeBack / fiveDoll;
 				changeBack = changeBack % fiveDoll;
-			System.out.println(Five + " five dollar bill(s)");
+				System.out.println(fives + " five dollar bill(s)");
 			
-			double One = changeBack / oneDoll;
+				int ones = 0;
+				ones = changeBack / oneDoll;
 				changeBack = changeBack % oneDoll;
-			System.out.println(One + " one dollar bill(s)");
-			
-			double Quarter = changeBack / quarter;
+				System.out.println(ones + " one dollar bill(s)");
+				
+				int quarters = 0;
+				quarters = changeBack / quarter;
 				changeBack = changeBack % quarter;
-			System.out.println(Quarter + " quarter(s)");
-			
-			double Dime = changeBack / dime;
+				System.out.println(quarters + " quarter(s)");
+				
+				int dimes = 0;
+				dimes = changeBack / dime;
 				changeBack = changeBack % dime;
-			System.out.println(Dime + " dime(s)");
-			
-			double Nickel = changeBack / nickel;
+				System.out.println(dimes + " dime(s)");
+				
+				int nickels = 0;
+				nickels = changeBack / nickel;
 				changeBack = changeBack % nickel;
-			System.out.println(nickel + " nickel(s)");
-			
-			double Penny = changeBack / penny;
+				System.out.println(nickels + " nickel(s)");
+				
+				int pennies = 0;
+				pennies = changeBack / penny;
 				changeBack = changeBack % penny;
-			System.out.println(penny + " penny(ies)");
-//			// dollar equations
-//			
-//			oneDoll = guestMoney - itemPrice+50;
-//				hundredDoll = oneDoll/100;
-//					oneDoll = oneDoll % 100;
-//					
-//				twentyDoll = oneDoll/20;
-//					oneDoll = oneDoll % 20;
-//			
-//				tenDoll = oneDoll/10;
-//					oneDoll = oneDoll % 10;
-//					
-//				fiveDoll = oneDoll/5;
-//					oneDoll = oneDoll % 5;
-//					
-//				oneDoll = oneDoll/1;
-//					oneDoll = oneDoll % 1;
-//					
-//					
-//			// change equations
-//			
-//			penny = guestMoney - itemPrice+50;
-//				oneDoll = penny/100;
-//					penny = penny % 100;
-//					
-//				quarter = penny/25;
-//					penny = penny % 25;
-//					
-//				dime = penny/10;
-//					penny = penny % 25;
-//					
-//				nickel = penny/5;
-//					penny = penny % 5;
-//					
-//				penny = penny/1;
-//					penny = penny % 1;
-//	
+				System.out.println(pennies + " penny(ies)");
+				changeGiven = true;
 		} 
 		else if (itemPrice != guestMoney) {
 			System.out.println("You entered less than the amount stated. "
 					+ "Please enter the amount needed: " );
 			guestMoney = kb.nextDouble();
 			
+		}
 		}
 		//limit needs to be made 20
 		kb.close();
@@ -117,4 +86,3 @@ public class MakeChange {
 		
 	} 
 	}
-	
